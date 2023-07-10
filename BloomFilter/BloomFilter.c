07df554a65c42598e073b73ad5235ca551b145a8
vaggelisarg13 <vag.argyropoulos@gmail.com>
2023-07-10 22:05:11 +0300

    small changes in the include files

--- BloomFilter/BloomFilter.c
+++ BloomFilter/BloomFilter.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 #include <string.h>
 #include <assert.h>
 #include "BloomFilter.h"
--- RedBlackTree/RedBlackTree.c
+++ RedBlackTree/RedBlackTree.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <assert.h>
 #include "RedBlackTree.h"
 
